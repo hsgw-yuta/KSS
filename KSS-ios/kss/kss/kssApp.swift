c57b549e470d1c886713b3ae0ae3431204106ef0
長谷川勇太 <hasegawayuta@hasegawayuutanoMacBook-Pro.local>
2021-07-05 00:24:42 +0900

    first commit

---
+++ KSS-ios/kss/kss/kssApp.swift
@@ -0,0 +1,17 @@
+//
+//  kssApp.swift
+//  kss
+//
+//  Created by 長谷川勇太 on 2021/07/05.
+//
+
+import SwiftUI
+
+@main
+struct kssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
